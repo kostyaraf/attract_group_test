ea413942aa2b5d1606bde793c6dabd409a82d634
k.rafalsky <k.rafalsky@splinestudio.ua>
2021-01-27 11:04:21 +0200

    [sidebar] created a SideBar component

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  Grid,
+  Box,
+  Typography,
+  FormControl,
+  Select,
+  MenuItem,
+} from '@material-ui/core/'
+import CitySelector from '../CitySelector'
+import CategoryCheckbox from '../CategoryCheckbox'
+
+class SideBar extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      data: props.data,
+      cities: props.cities,
+      categories: props.categories,
+    }
+  }
+  render() {
+    console.log(this.props)
+    return (
+      <div style={{ maxWidth: 300 }}>
+        <Box>
+          <CitySelector title='City' data={this.props.cities} setFilter={this.props.setFilter}/>
+          <CategoryCheckbox title='Category' data={this.props.categories} setFilter={this.props.setFilter}/>
+
+          {/* <Typography variant='h5'>CATEGORIES</Typography> */}
+
+        </Box>
+      </div>
+    )
+  }
+}
+
+export default SideBar
