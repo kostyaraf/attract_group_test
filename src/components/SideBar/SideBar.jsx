4c9825def582b73edb9dfd7287a26613983a9257
k.rafalsky <k.rafalsky@splinestudio.ua>
2021-01-27 11:49:03 +0200

    [sidebar] added CategoryCheckbox

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -13,22 +13,30 @@ import CategoryCheckbox from '../CategoryCheckbox'
 class SideBar extends React.Component {
   constructor(props) {
     super()
-    this.state = {
-      data: props.data,
-      cities: props.cities,
-      categories: props.categories,
-    }
+    // this.state = {
+    //   data: props.data,
+    //   cities: props.cities,
+    //   categories: props.categories,
+    // }
   }
   render() {
     console.log(this.props)
     return (
       <div style={{ maxWidth: 300 }}>
         <Box>
-          <CitySelector title='City' data={this.props.cities} setFilter={this.props.setFilter}/>
-          <CategoryCheckbox title='Category' data={this.props.categories} setFilter={this.props.setFilter}/>
+          <CitySelector
+            title='City'
+            data={this.props.state.cities}
+            setFilter={this.props.setFilter}
+          />
+          <CategoryCheckbox
+            title='Category'
+            data={this.props.categories}
+            setFilter={this.props.setFilter}
+            filter={this.props.state.filter.categories}
+          />
 
           {/* <Typography variant='h5'>CATEGORIES</Typography> */}
-
         </Box>
       </div>
     )
